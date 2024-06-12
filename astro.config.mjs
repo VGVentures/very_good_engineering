0206a6689939018d2e2d437341e961ef2d046aac
Joanna May <joannamadisonmay@gmail.com>
2024-06-12 15:52:35 -0500

    feat: initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,79 @@
+import mdx from "@astrojs/mdx";
+import react from "@astrojs/react";
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      favicon: "./public/favicon.png",
+      title: "Very Good Practices",
+      head: [
+        {
+          // Fix theme flickering on page load.
+          // See https://scottwillsey.com/theme-flicker/
+          tag: "script",
+          attrs: {
+            src: `
+            <script>
+              const theme = localStorage.getItem("theme") || "light";
+              document.documentElement.dataset.theme = theme;
+            </script>
+            `,
+          },
+        },
+      ],
+      customCss: [
+        // Add tailwind base styles:
+        "./src/assets/styles/tailwind.css",
+        "@fontsource/poppins/100.css",
+        "@fontsource/poppins/200.css",
+        "@fontsource/poppins/300.css",
+        "@fontsource/poppins/400.css",
+        "@fontsource/poppins/500.css",
+        "@fontsource/poppins/600.css",
+        "@fontsource/poppins/700.css",
+        "@fontsource/poppins/800.css",
+        "@fontsource/poppins/900.css",
+      ],
+      editLink: {
+        baseUrl:
+          "https://github.com/verygoodopensource/very_good_practices/edit/main/docs/",
+      },
+      logo: {
+        light: "./src/assets/logos/unicorn_light.png",
+        dark: "./src/assets/logos/unicorn_dark.png",
+      },
+      social: {
+        github: "https://github.com/verygoodopensource/very_good_practices",
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            {
+              label: "Example Guide",
+              link: "/guides/example/",
+            },
+          ],
+        },
+        {
+          label: "Reference",
+          autogenerate: {
+            directory: "reference",
+          },
+        },
+      ],
+    }),
+    react(),
+    mdx(),
+    tailwind({
+      // Disable the default base styles:
+      applyBaseStyles: false,
+    }),
+  ],
+});
---
+++ eslint.config.mjs
@@ -0,0 +1,15 @@
+import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginAstro from "eslint-plugin-astro";
+
+export default [
+  // add more generic rule sets here, such as:
+  // js.configs.recommended,
+  ...eslintPluginAstro.configs.recommended,
+  eslintConfigPrettier,
+  {
+    rules: {
+      // override/add rules settings here, such as:
+      // "astro/no-set-html-directive": "error"
+    },
+  },
+];
