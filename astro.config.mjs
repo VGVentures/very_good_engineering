1abcd37550a6d40ee7360b28cd8b834d0e9e9a83
Mark <marwfair@gmail.com>
2024-06-14 11:20:03 -0500

    feat: add deploy workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://VGVentures.github.io",
   integrations: [
     starlight({
       favicon: "./public/favicon.png",
