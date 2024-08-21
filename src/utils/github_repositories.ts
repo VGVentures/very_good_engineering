0c1ae2b734094bd1e087d075522161f85b52f5b6
Joanna May <joannamadisonmay@gmail.com>
2024-08-21 13:03:53 -0500

    wip: very good development section

--- src/utils/github_repositories.ts
+++ src/utils/github_repositories.ts
@@ -25,58 +25,117 @@ export const organizationName = "VGVentures";
 // GitHub actions will run accordingly.
 export const VGVENTURES_GITHUB_PAT = "VGVENTURES_GITHUB_PAT";
 
-var repositoryCache: GithubRepositories | undefined;
+const octokit = new Octokit({ auth: process.env[VGVENTURES_GITHUB_PAT] });
 
+var vgvRepositoryCache: GithubRepositories | undefined;
+var repositoryCache: GithubRepositories = {};
+
+/**
+ * Gets all public repositories from the VGVentures organization. If already
+ * fetched, returns the cached repository data.
+ * @returns A map of repository full names (org/repo) to repository data.
+ */
 export async function getRepositories(): Promise<GithubRepositories> {
-  if (repositoryCache === undefined) {
-    repositoryCache =
+  if (vgvRepositoryCache === undefined) {
+    vgvRepositoryCache =
       await fetchOrganizationPublicRepositories(organizationName);
   }
 
-  return repositoryCache;
+  return vgvRepositoryCache;
+}
+
+/**
+ * Gets an individual repository. If already fetched, returns the cached
+ * repository data.
+ * @param owner Repository owner, e.g. "VGVentures"
+ * @param repo Repository name: e.g., "bloc_concurrency_demos"
+ * @returns Repository information.
+ */
+export async function getRepository(
+  owner: string,
+  repo: string,
+): Promise<GithubRepository> {
+  const fullName = `${owner}/${repo}`;
+  if (owner == organizationName) {
+    return (await getRepositories())[fullName]!;
+  }
+
+  if (typeof repositoryCache[fullName] === "undefined") {
+    repositoryCache[fullName] = await fetchRepository(owner, repo);
+  }
+
+  return repositoryCache[fullName];
 }
 
 // Returns a map of repository full names to repository data.
 async function fetchOrganizationPublicRepositories(
   organizationName: string,
 ): Promise<GithubRepositories> {
-  const octokit = new Octokit({ auth: process.env[VGVENTURES_GITHUB_PAT] });
-
   // Get EVERY public repo in the organization.
-  const response = await octokit.repos.listForOrg({
-    type: "public",
-    org: organizationName,
-  });
+  const response = await octokit.paginate(
+    octokit.repos.listForOrg,
+    {
+      type: "public",
+      per_page: 100,
+      org: organizationName,
+      headers: {
+        "If-None-Match": "",
+      },
+    },
+    (response) => response.data,
+  );
 
-  if (typeof response !== "object" || !Array.isArray(response["data"])) {
+  if (!Array.isArray(response)) {
     throw new Error(
       "Unexpected response from GitHub API: " +
         JSON.stringify(response, null, 2),
     );
   }
 
-  const data = response["data"];
-
   // Return a map of repository full names to repository data.
   var repos: GithubRepositories = {};
-  for (const raw of data) {
-    const language = raw["language"];
-    const languageColor =
-      language != null ? languageColors[language] ?? null : null;
-    const repo: GithubRepository = {
-      name: raw["name"],
-      organization: organizationName,
-      url: raw["html_url"],
-      fullName: raw["full_name"],
-      description: raw["description"] ?? "",
-      stars: raw["stargazers_count"] ?? 0,
-      forks: raw["forks"] ?? 0,
-      language: raw["language"] ?? "",
-      languageColor: languageColor,
-    };
+  for (const raw of response) {
+    const repo = mapRepository(raw);
     repos[repo.fullName] = repo;
     console.log("GitHub Repository: " + JSON.stringify(repo, null, 2));
   }
 
   return repos;
+}
+
+async function fetchRepository(
+  owner: string,
+  repo: string,
+): Promise<GithubRepository> {
+  // fetch single repo info with octokit
+  const response = await octokit.repos.get({
+    owner,
+    repo,
+  });
+
+  if (typeof response !== "object") {
+    throw new Error(
+      "Unexpected response from GitHub API: " +
+        JSON.stringify(response, null, 2),
+    );
+  }
+
+  return mapRepository(response);
+}
+
+function mapRepository(raw: { [key: string]: any }): GithubRepository {
+  const language = raw["language"];
+  const languageColor =
+    language != null ? languageColors[language] ?? null : null;
+  return {
+    name: raw["name"],
+    organization: organizationName,
+    url: raw["html_url"],
+    fullName: raw["full_name"],
+    description: raw["description"] ?? "",
+    stars: raw["stargazers_count"] ?? 0,
+    forks: raw["forks"] ?? 0,
+    language: raw["language"] ?? "",
+    languageColor: languageColor,
+  };
 }
