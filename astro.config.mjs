bdffa9d195c68f292f98c677035a2efb7d1cb4ad
Joanna May <joannamadisonmay@gmail.com>
2024-09-16 13:11:56 -0500

    chore: revert unnecessary package

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import starlight from "@astrojs/starlight";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
-import relativeLinks from "astro-relative-links";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://vgventures.github.io",
@@ -136,6 +134,5 @@ export default defineConfig({
       // Disable the default base styles:
       applyBaseStyles: false,
     }),
-    relativeLinks(),
   ],
 });
