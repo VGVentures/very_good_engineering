27231f8f90f1f642566ff23d76e03f9d50ea56ed
Marcus Twichel <marcus@verygood.ventures>
2025-04-07 09:18:18 -0600

    format

--- src/pages/llms-full.txt.ts
+++ src/pages/llms-full.txt.ts
@@ -4,12 +4,12 @@ import type { APIRoute } from "astro";
 const docs = await getCollection("docs");
 
 export const GET: APIRoute = async () => {
-    return new Response(
-        `# Very Good Engineering Full Documentation\n\n${docs
-            .map((doc) => {
-                return `# ${doc.data.title}\n\n${doc.body}\n\n`;
-            })
-            .join("")}`,
-        { headers: { "Content-Type": "text/plain; charset=utf-8" } }
-    );
+  return new Response(
+    `# Very Good Engineering Full Documentation\n\n${docs
+      .map((doc) => {
+        return `# ${doc.data.title}\n\n${doc.body}\n\n`;
+      })
+      .join("")}`,
+    { headers: { "Content-Type": "text/plain; charset=utf-8" } },
+  );
 };
